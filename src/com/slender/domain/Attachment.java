25aea38d85e13996a23acc5d7a64e7ccf5e64aa0
Nick <nicwessels@gmail.com>
2014-11-14 19:33:54 +0200

    New Javadocs added.

--- src/com/slender/domain/Attachment.java
+++ src/com/slender/domain/Attachment.java
@@ -12,6 +12,10 @@ import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 import org.hibernate.annotations.Entity;
 
+/**
+ *
+ * @author Nick
+ */
 @Entity
 @XmlRootElement
 public class Attachment implements Serializable{
@@ -23,58 +27,114 @@ public class Attachment implements Serializable{
     private Integer taskId;
     private Integer userId;
 
+    /**
+     *
+     * @return
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getAttachmentName() {
         return attachmentName;
     }
 
+    /**
+     *
+     * @param attachmentName
+     */
     public void setAttachmentName(String attachmentName) {
         this.attachmentName = attachmentName;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getAttachment() {
         return attachment;
     }
 
+    /**
+     *
+     * @param attachment
+     */
     public void setAttachment(String attachment) {
         this.attachment = attachment;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getCreateDate() {
         return createDate;
     }
 
+    /**
+     *
+     * @param createDate
+     */
     public void setCreateDate(Date createDate) {
         this.createDate = createDate;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getProjectId() {
         return projectId;
     }
 
+    /**
+     *
+     * @param projectId
+     */
     public void setProjectId(Integer projectId) {
         this.projectId = projectId;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getTaskId() {
         return taskId;
     }
 
+    /**
+     *
+     * @param taskId
+     */
     public void setTaskId(Integer taskId) {
         this.taskId = taskId;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getUserId() {
         return userId;
     }
 
+    /**
+     *
+     * @param userId
+     */
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
